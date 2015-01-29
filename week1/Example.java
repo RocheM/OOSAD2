29f6e019e91dea02cff76773d2bfd3e917ace384
RocheM <matthew.the.roche@gmail.com>
2015-01-29 12:35:48 +0000

    This was created using VP

---
+++ week1/Example.java
@@ -0,0 +1,38 @@
+public class Example {
+
+	private string Name;
+	private int attribute;
+
+	public Example() {
+		// TODO - implement Example.Example
+		throw new UnsupportedOperationException();
+	}
+
+	public string getName() {
+		// TODO - implement Example.getName
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param Name
+	 */
+	public void setName(string Name) {
+		// TODO - implement Example.setName
+		throw new UnsupportedOperationException();
+	}
+
+	public void getAttribute() {
+		// TODO - implement Example.getAttribute
+		throw new UnsupportedOperationException();
+	}
+
+	/**
+	 * 
+	 * @param attribute
+	 */
+	public void setAttribute(int attribute) {
+		this.attribute = attribute;
+	}
+
+}
